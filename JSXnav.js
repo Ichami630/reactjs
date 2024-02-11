9e3ea3f63bd34e71761be1cc4cc90040b6549a96
Ichami630 <110008066+Ichami630@users.noreply.github.com>
2024-02-11 16:03:57 +0000

    Create JSXnav.js

---
+++ JSXnav.js
@@ -0,0 +1,12 @@
+  const page=(
+      <nav className='navbar'>
+        <h1>Ichami</h1>
+        <ul>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+  );
+  ReactDOM.render(page,document.getElementById("root"));
+  
